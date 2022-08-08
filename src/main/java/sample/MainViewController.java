9e9075dcb00ebcea114f92ba7e388bada809bcb5
Anna3001 <aniaw3001@icloud.com>
2022-08-08 20:22:57 +0200

    Adding button operation

--- src/main/java/sample/MainViewController.java
+++ src/main/java/sample/MainViewController.java
@@ -1,13 +1,62 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 public class MainViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    private Label expression;
+
+    public void insertNumber(String number) {
+        expression.setText(expression.getText() + number);
+    }
+
+    public void insertOperator(String operator) {
+        expression.setText(expression.getText() + " " + operator + " ");
+    }
+
+    public void clearExpression() {
+        expression.setText("");
+    }
+    public void onMouseClick(MouseEvent mouseEvent) {
+
+        Button button = (Button) mouseEvent.getSource();
+        String buttonText = button.getText();
+
+        switch(buttonText) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                insertNumber(buttonText);
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                insertOperator(buttonText);
+                break;
+            case "C":
+                clearExpression();
+                break;
+        }
     }
 }

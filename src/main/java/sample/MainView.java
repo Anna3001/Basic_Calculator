6514fe6dda1f0b9bd487edc33ff52331c6d7aa0a
Anna3001 <aniaw3001@icloud.com>
2022-08-08 20:24:38 +0200

    Changing the way of loading FXML file

--- src/main/java/sample/MainView.java
+++ src/main/java/sample/MainView.java
@@ -3,14 +3,14 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class MainView extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainViewInterface.fxml")))));
+        BorderPane borderPane = FXMLLoader.load(getClass().getResource("/MainViewInterface.fxml"));
+        stage.setScene(new Scene(borderPane));
         stage.setResizable(false);
         stage.setTitle("Basic Calculator");
         stage.show();

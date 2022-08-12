92f4b2d0fbb738bb5f06b4ebec4cb4e13d8299d0
Anna3001 <aniaw3001@icloud.com>
2022-08-12 17:22:01 +0200

    Adding javafx.base

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Basic.Calculator {
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.base;
 
     exports sample to javafx.graphics, javafx.fxml;
     opens sample to javafx.fxml;

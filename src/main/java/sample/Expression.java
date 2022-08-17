f8552156b3b0f691ccd96b10a3331bff5e92653c
Anna3001 <aniaw3001@icloud.com>
2022-08-17 22:54:14 +0200

    Adding a check value of numbers

--- src/main/java/sample/Expression.java
+++ src/main/java/sample/Expression.java
@@ -3,11 +3,13 @@ package sample;
 import java.util.Stack;
 
 public class Expression {
+    private static double result;
+    private static boolean isOperator;
 
-    public static int evaluate(String expression) {
+    public static Double evaluate(String expression) {
 
         char[] symbolsOfExpr = expression.toCharArray();
-        Stack<Integer> numbers = new Stack<Integer>();
+        Stack<Double> numbers = new Stack<Double>();
         Stack<Character> operators = new Stack<Character>();
 
         for (int i = 0; i < symbolsOfExpr.length; i++) {
@@ -29,23 +31,56 @@ public class Expression {
                     }
                 }
 
-                numbers.push(Integer.parseInt(stringBuffer.toString()));
+                numbers.push(Double.valueOf(stringBuffer.toString()));
 
+                isOperator = false;
             } else if (symbolsOfExpr[i] == '-' || symbolsOfExpr[i] == '+' || symbolsOfExpr[i] == '*' || symbolsOfExpr[i] == '/') {
-
-                while (!operators.empty() && hasPrecedence(symbolsOfExpr[i], operators.peek())) {
-                    numbers.push(applyOperator(operators.pop(), numbers.pop(), numbers.pop()));
+                if(!isOperator) {
+                    while (!operators.empty() && hasPrecedence(symbolsOfExpr[i], operators.peek())) {
+                        if (!numbers.empty()) {
+                            if ((result = applyOperator(operators.pop(), numbers.pop(), numbers.pop())) != 0) {
+                                if (result < 10000000.0) {
+                                    numbers.push(result);
+                                } else if (result == 10000002.0) {
+                                    return 10000002.0;
+                                } else {
+                                    return 10000000.0;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    return 10000001.0;
                 }
-
                 operators.push(symbolsOfExpr[i]);
+                isOperator = true;
             }
         }
 
-        while (!operators.empty()) {
-            numbers.push(applyOperator(operators.pop(), numbers.pop(), numbers.pop()));
+        if(numbers.size() == 1){
+            return 10000001.0;
         }
 
-        return numbers.pop();
+            while (!operators.empty()) {
+                if(!numbers.empty()) {
+                    if ((result = applyOperator(operators.pop(), numbers.pop(), numbers.pop())) != 0) {
+                        if (result < 10000000.0) {
+                            numbers.push(result);
+                        } else if (result == 10000002.0) {
+                            return 10000002.0;
+                        } else {
+                            return 10000000.0;
+                        }
+                    }
+                }
+            }
+
+
+        if(!numbers.empty()) {
+            return numbers.pop();
+        } else {
+            return 10000000.0;
+        }
     }
 
     public static boolean hasPrecedence(char oper1, char oper2) {
@@ -56,7 +91,11 @@ public class Expression {
             return true;
     }
 
-    public static int applyOperator(char oper, int nb2, int nb1) {
+    public static double applyOperator(char oper, Double nb2, Double nb1) {
+
+        if(nb1 > 10000 || nb2 > 10000) {
+            return 10000002.0;
+        }
 
         switch (oper)
         {
